aa5bdaef28de6c4a75e9105008de92ef1bbb7ace
surface <andy_gejunfei@163.com>
2017-12-25 00:29:25 +0800

    improve the aliens

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,7 @@
 
 import sys
 import pygame
+from time import sleep
 from bullet import Bullet
 from alien import Alien
 
@@ -106,12 +107,48 @@ def check_fleet_edges(ai_settings,aliens):
             change_fleet_direction(ai_settings,aliens)
             break
 
-def update_aliens(ai_settings,aliens):
+def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
+    """响应被外星人撞到的飞船"""
+    if stats.ships_left > 0:
+        # 将ship_left减1
+        stats.ships_left -= 1
+
+        # 清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty()
+
+        # 创建一群新的外星人，并将飞船放到屏幕低端中央
+        create_fleet(ai_settings,screen,ship,aliens)
+        ship.center_ship()
+
+        # 暂停
+        sleep(0.5)
+
+    else:
+        stats.game_active = False
+
+def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
+    """检查是否有外星人到达了屏幕底端"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # 像飞船被撞到一样进行处理
+            ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+            break
+
+def update_aliens(ai_settings,stats,screen,ship,aliens,bullets):
     """
     检查是否有外星人位于屏幕边缘，并更新外星人群中所有外星人的位置
     """
     check_fleet_edges(ai_settings,aliens)
     aliens.update()
+
+    # 检测外星人和飞船之间的碰撞
+    if pygame.sprite.spritecollideany(ship,aliens):
+        ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+
+    # 检查是否有外星人到达屏幕底端
+    check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets)
 
 def update_screen(ai_settings,screen,ship,aliens,bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
--- settings.py
+++ settings.py
@@ -6,12 +6,13 @@ class Settings():
     def __init__(self):
         """初始化游戏设置"""
         # 屏幕设置
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 900
+        self.screen_height = 600
         self.bg_color = (230,230,230)
 
-        # 飞船速度的设置
+        # 飞船设置
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
 
         # 子弹设置
         self.bullet_speed_factor = 1
